300499b8bc5948cf804aae78bb9fa78d689f46ac
VAN2for <90973590+2389927630@users.noreply.github.com>
2025-05-04 18:15:55 +0800

    更新文字对话

--- src/components/Chat.vue
+++ src/components/Chat.vue
@@ -48,9 +48,9 @@ let textList=reactive<{ from: string; value: string }[]>([
 ])
 
 
-let sdkAppId=1600085063
+let sdkAppId=20022655;
 console.log(sdkAppId+"这是sdkAppId")
-let sdkSecretKey='e7de5690dea59142e08f481af44f6573b5f1f551a58f91bd71cb2a9269a649f6'
+let sdkSecretKey='352a708f07e30c918fe120f0e8a5327c69b19b74f2eed513b3ee9c8aea0133a2'
 let options = {
     SDKAppID: sdkAppId // 接入时需要将0替换为您的即时通信 IM 应用的 SDKAppID
 };
